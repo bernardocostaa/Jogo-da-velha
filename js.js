1ff48d38ac7be6c4edd1b4cc3f23e2cfc351d115
Bernardo Costa <bernardo-v.f@hotmail.com>
2023-03-24 23:19:38 -0300

    js + css

--- js.js
+++ js.js
@@ -4,8 +4,28 @@ const btnReset = document.getElementById('reset')
 
 let isX = false
 
-function reset(){
+reset()
+function randomGame(){
+  let random = Math.floor(Math.random() * 2)
+  area.classList.remove('x')
+  area.classList.remove('circulo')
+  if(random === 0){
+    isX = true
+    console.log('1');
+    area.classList.add('x')
+  }else{
+    isX = false
+    area.classList.add('circulo')
+    console.log('2');
+  }
+}
 
+function reset(){
+  quadroGame.forEach((item)=>{
+    item.classList.remove('x')
+    item.classList.remove('circulo')
+  })
+  randomGame()
 }
 
 function  marcarTela(el,classe){
@@ -14,16 +34,16 @@ function  marcarTela(el,classe){
 
 function eventoClique(e){
   const quadro = e.target
+  if(quadro.classList.contains('x') || quadro.classList.contains('circulo')){
+    return;
+  }
   const addClassTela = isX ? 'x': 'circulo'
-
   marcarTela(quadro,addClassTela)
-
   trocaTurno()
 }
 
 function trocaTurno(){
   isX = !isX
-  console.log(isX);
   area.classList.remove('circulo')
   area.classList.remove('x')
   
@@ -31,11 +51,10 @@ function trocaTurno(){
     area.classList.add('x')
   }else{
     area.classList.add('circulo')
-
   }
 }
 
 quadroGame.forEach((item)=>{
-  item.addEventListener('click',eventoClique, {once:true})
+  item.addEventListener('click',eventoClique)
 })
 btnReset.addEventListener('click',reset)
