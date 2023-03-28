b20d7cf80296c9f872925aa0a45602be7dc918eb
Bernardo Costa <bernardo-v.f@hotmail.com>
2023-03-28 16:17:39 -0300

    jogo da velha

--- js.js
+++ js.js
@@ -1,70 +1,118 @@
-const quadroGame = document.querySelectorAll('[data-quadro]')
-const area = document.querySelector('.area')
-const btnReset = document.getElementById('reset')
-const vez = document.getElementById('qualJogador')
+const quadroGame = document.querySelectorAll("[data-quadro]");
+const area = document.querySelector(".area");
+const btnReset = document.getElementById("reset");
+const btnNewGame = document.getElementById("newGame");
+const vez = document.getElementById("qualJogador");
+const modal = document.querySelector(".modal-cont");
 
-let isX = false
-let jogador = ''
+let isX = false;
+let jogador = "";
+const tabelaWin = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-reset()
-function randomGame(){
-  let random = Math.floor(Math.random() * 2)
-  area.classList.remove('x')
-  area.classList.remove('circulo')
-  if(random === 0){
-    isX = true
-    jogador = 'X'
-    area.classList.add('x')
-  }else{
-    isX = false
-    jogador = 'O'
-    area.classList.add('circulo')
+reset();
+function randomGame() {
+  let random = Math.floor(Math.random() * 2);
+  area.classList.remove("x");
+  area.classList.remove("circulo");
+  if (random === 0) {
+    isX = true;
+    jogador = "X";
+    area.classList.add("x");
+  } else {
+    isX = false;
+    jogador = "O";
+    area.classList.add("circulo");
   }
-  rederInfo()
+  rederInfo();
 }
 
-function reset(){
-  quadroGame.forEach((item)=>{
-    item.classList.remove('x')
-    item.classList.remove('circulo')
-  })
-  randomGame()
+function reset() {
+  quadroGame.forEach((item) => {
+    item.classList.remove("x");
+    item.classList.remove("circulo");
+  });
+  randomGame();
 }
 
-function  marcarTela(el,classe){
-  el.classList.add(classe)
+function marcarTela(el, classe) {
+  el.classList.add(classe);
 }
 
-function eventoClique(e){
-  const quadro = e.target
-  if(quadro.classList.contains('x') || quadro.classList.contains('circulo')){
+function eventoClique(e) {
+  const quadro = e.target;
+  const addClassTela = isX ? "x" : "circulo";
+  if (quadro.classList.contains("x") || quadro.classList.contains("circulo")) {
     return;
   }
-  const addClassTela = isX ? 'x': 'circulo'
-  marcarTela(quadro,addClassTela)
-  trocaTurno()
-}
+  marcarTela(quadro, addClassTela);
 
-function trocaTurno(){
-  isX = !isX
-  area.classList.remove('circulo')
-  area.classList.remove('x')
-  
-  if(isX){
-    jogador = 'X'
-    area.classList.add('x')
+  const ganhou = checkWin(addClassTela);
+  const empate = checkEmpate()
+  if (ganhou) {
+    fimDeJogo(false)
+  }else if(empate){
+    fimDeJogo(true)
   }else{
-    jogador = 'O'
-    area.classList.add('circulo')
+    trocaTurno();
   }
-  rederInfo()
 }
 
-function rederInfo(){
-  vez.innerText = jogador
+function trocaTurno() {
+  isX = !isX;
+  area.classList.remove("circulo");
+  area.classList.remove("x");
+
+  if (isX) {
+    jogador = "X";
+    area.classList.add("x");
+  } else {
+    jogador = "O";
+    area.classList.add("circulo");
+  }
+  rederInfo();
 }
 
-quadroGame.forEach((item)=>{
-  item.addEventListener('click',eventoClique)
-})
-btnReset.addEventListener('click',reset)
+function rederInfo() {
+  vez.innerText = jogador;
+}
+
+function checkWin(jogadorNoMomento) {
+  return tabelaWin.some((conbinacao) => {
+    return conbinacao.every((index) => {
+      return quadroGame[index].classList.contains(jogadorNoMomento);
+    });
+  });
+}
+function checkEmpate(){
+  return [...quadroGame].every((item)=>{
+   return item.classList.contains('x') ||  item.classList.contains('circulo')
+  })
+}
+
+function fimDeJogo(empate) {
+  const finalMsg = document.querySelector('.finalMsg')
+  if (empate) {
+    finalMsg.innerText = 'Empate!';
+  }else{
+    finalMsg.innerText = isX ? 'X venceu!' : 'O venceu!'
+  }
+  modal.classList.add('ativo')
+}
+
+quadroGame.forEach((item) => {
+  item.addEventListener("click", eventoClique);
+});
+btnReset.addEventListener("click", reset);
+btnNewGame.addEventListener("click", () => {
+  modal.classList.remove("ativo");
+  reset();
+});
